63f33422f72d482e82d33cc2220aa69f5acdf6a7
Arjun Baokar <arjunbaokar@gmail.com>
2012-09-29 17:20:07 -0700

    added tentative filepaths for 10/14 images

--- src/NavBar.java
+++ src/NavBar.java
@@ -15,22 +15,25 @@ public class NavBar extends JPanel implements MouseListener, MouseMotionListener
 	BufferedImage homeTitle, statsTitle, logTitle, settingsTitle;
 	BufferedImage butPress1, butPress2, butPress3, butPress4;
 	BufferedImage butUnpress1, butUnpress2, butUnpress3, butUnpress4;
+	BufferedImage dayBg, nightBg;
 	
 	public NavBar(){
 		super();
 		try{
-			homeTitle = ImageIO.read( new File("INSERTIMAGENAME") );
+			/* homeTitle = ImageIO.read( new File("INSERTIMAGENAME") );
 			statsTitle = ImageIO.read( new File("INSERTIMAGENAME") );
 			logTitle = ImageIO.read( new File("INSERTIMAGENAME") );
-			settingsTitle = ImageIO.read( new File("INSERTIMAGENAME") );
-			butPress1 = ImageIO.read( new File("INSERTIMAGENAME") );
-			butPress2 = ImageIO.read( new File("INSERTIMAGENAME") );
-			butPress3 = ImageIO.read( new File("INSERTIMAGENAME") );
-			butPress4 = ImageIO.read( new File("INSERTIMAGENAME") );
-			butUnpress1 = ImageIO.read( new File("INSERTIMAGENAME") );
-			butUnpress2 = ImageIO.read( new File("INSERTIMAGENAME") );
-			butUnpress3 = ImageIO.read( new File("INSERTIMAGENAME") );
-			butUnpress4 = ImageIO.read( new File("INSERTIMAGENAME") );
+			settingsTitle = ImageIO.read( new File("INSERTIMAGENAME") ); */
+			butPress1 = ImageIO.read( new File("images/home_nav_button_depressed.png") );
+			butPress2 = ImageIO.read( new File("images/stats_nav_button_depressed.png") );
+			butPress3 = ImageIO.read( new File("images/sleepLog_nav_button_depressed.png") );
+			butPress4 = ImageIO.read( new File("images/settings_nav_button_depressed.png") );
+			butUnpress1 = ImageIO.read( new File("images/home_nav_button_inactive.png") );
+			butUnpress2 = ImageIO.read( new File("images/stats_nav_button_inactive.png") );
+			butUnpress3 = ImageIO.read( new File("images/sleepLog_nav_button_inactive.png") );
+			butUnpress4 = ImageIO.read( new File("images/settings_nav_button_inactive.png") );
+			dayBg = ImageIO.read( new File("images/background_day.png") );
+			nightBg = ImageIO.read( new File("images/background_night.png") );
 		} catch (IOException e){
 			e.printStackTrace();
 		}
@@ -41,7 +44,6 @@ public class NavBar extends JPanel implements MouseListener, MouseMotionListener
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		
 		
 		mouseClicked = false;
 		mouseMoved = false;
